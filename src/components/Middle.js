66ce79e2ee7e02f8d1804944d521ca9c3f0aa6dd
Vikrant <vikrantp758@gmail.com>
2021-06-03 03:30:19 +0530

    Added Left side And Header

---
+++ src/components/Middle.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+import React from 'react'
+
+const Middle = () => {
+    return (
+        <Container>Middle</Container>
+
+    )
+}
+
+const Container = styled.div`
+grid-area: middle;
+`;
+
+export default Middle
---
+++ src/components/RightSide.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+import React from 'react'
+
+const RightSide = () => {
+    return (
+        <Container>
+            <FollowCard>
+                <Title>
+                    <h2>Add to your feed</h2>
+                    <img src="/images/feed-icon.svg" alt="feed" />
+                </Title>
+            </FollowCard>
+        </Container>
+
+    )
+}
+
+const Container = styled.div`
+grid-area: rightside;
+
+`;
+
+const FollowCard = styled.div`
+text-align: center;
+overflow: hidden;
+margin-bottom: 8px;
+background-color: #fff;
+border-radius: 5px;
+position: relative;
+border: none;
+box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
+padding: 12px;
+
+`;
+
+const Title = styled.div`
+display: inline-flex;
+align-items: center;
+justify-content: space-between;
+font-size: 16px;
+width: 100%;
+color: rgba(0,0,0,0.6);
+`;
+
+export default RightSide
