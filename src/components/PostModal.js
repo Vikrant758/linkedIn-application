9d5502d50ec48d1aa5c238c1b9cd8363366a8d62
Vikrant <vikrantp758@gmail.com>
2021-06-11 02:43:53 +0530

    Updates in PostModal

--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -4,7 +4,23 @@ import styled from 'styled-components';
 const PostModal = (props) => {
     return (
         <Container>
-            PostMOdal
+            <Content>
+                <Header>
+                    <h2>Create a post</h2>
+                    <button>
+                        <img src="/images/cancel.svg" alt="cancel" />
+                    </button>
+                </Header>
+                <SharedContent>
+                    <UserInfo>
+                        <img src="/images/user.svg" alt="" />
+                        <span>Name</span>
+                    </UserInfo>
+                </SharedContent>
+                <ShareCreations>
+                    Actions
+                </ShareCreations>
+            </Content>
         </Container>
     )
 };
@@ -20,4 +36,83 @@ color: black;
 background-color: rgba(0, 0, 0, 0.8);
 `;
 
+const Content = styled.div`
+width: 100%;
+max-width: 552px;
+background-color: white;
+max-height: 90%;
+overflow: initial;
+border-radius: 5px;
+position: relative;
+display: flex;
+flex-direction: column;
+top: 32px;
+margin: 0 auto;
+`;
+
+const Header = styled.div`
+display: block;
+padding: 16px 20px;
+border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+font-size: 16px;
+line-height: 1.5;
+color: rgba(0, 0, 0, 0.6);
+font-weight: 400;
+display: flex;
+justify-content: space-between;
+align-items: center;
+button {
+    height: 40px;
+    width: 40px;
+    min-width: auto;
+    border: none;
+    background-color: white;
+    color: rgba(0, 0, 0, 0.15);
+    &:hover {
+        background-color: rgba(0, 0, 0, 0.09);
+        border-radius: 50%;
+    }
+    svg{
+        pointer-events: none;
+    }
+
+}
+
+`;
+
+const SharedContent = styled.div`
+display: flex;
+flex-direction: column;
+flex-grow: 1;
+overflow-y: auto;
+vertical-align: baseline;
+background: transparent;
+padding: 8px 12px;
+
+`;
+
+const UserInfo = styled.div`
+display: flex;
+align-items: center;
+padding: 12px 24px;
+img {
+    width: 48px;
+    height: 48px;
+    background-clip: content-box;
+    border: 2px solid transparent;
+    border-radius: 50%;
+}
+
+span {
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 1.5;
+    margin-left: 5px;
+
+}
+`;
+const ShareCreations = styled.div`
+
+`;
 export default PostModal
+
